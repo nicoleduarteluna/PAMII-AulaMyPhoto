e7dd559c8e677ca6a5de5da5395e83b0d0afe514
niluiz <nicoleduarteluna@hotmail.com>
2019-09-16 22:10:10 -0300

    Intent: tirar e visualizar foto

---
+++ app/src/main/java/br/com/etechoracio/MainActivity.java
@@ -0,0 +1,66 @@
+package br.com.etechoracio;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    Uri uri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onCapturar(View view)
+    {
+
+        File diretorio = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        String nomeImagem = diretorio.getPath() + "/" + System.currentTimeMillis() + ".jpg";
+        uri = Uri.fromFile(new File(nomeImagem));
+
+        /*uri = FileProvider.getUriForFile(
+                MainActivity.this,
+                "br.com.etechoracio.provider",
+                new File(nomeImagem));*/
+
+        Intent intent = new Intent (MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+        startActivityForResult(intent, 100);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if (requestCode == 100 && resultCode == RESULT_OK){
+            Toast.makeText(this, "Imagem capturada!", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+            intent.setData(uri);
+            this.sendBroadcast(intent);
+        }else {
+            Toast.makeText(this, "Imagem não capturada!", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    public void onVisualizar (View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setDataAndType(uri, "image/jpeg");
+        startActivity(intent);
+    }
+
+}
